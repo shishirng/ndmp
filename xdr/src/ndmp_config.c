11788f4913344a4202edce303784a5f59eccb07c
shrinidhihudli <shrinidhi@hudli.com>
2013-03-05 12:43:49 +0530

    Updated a few files
    Overall structure:
    The NDMP server is split into different components which are subdirectories of ndmp - currently comm, utils. In each component the source code is in a src directory. comm is reponsible for actual client-server communication, utils provides thread-safe queues and hex dumps.
    
    comm/src/comm.c, comm/src/comm.h:
    Structures and functions for listening and management of client connections/requests. Currently listens for connections from multiple clients (using select() system call). New connections are added to a list of sessions. Requests from connected clients are enqueued into a job queue.
    
    comm/src/worker.c, comm/src/worker.h:
    Thread creation, shutdown, running jobs from request queue, and processing jobs from response queue.
    
    utils/src/hexdump.c, utils/sec/hexdump.h:
    Hexdump utility.
    
    utils/src/locks.c, utils/src/locks.h:
    Provides mutex locks to make queues thread-safe (spinlock capability will be added in a future commit).
    
    utils/src/queue.c, utils/src/queue.h:
    Thread-safe queue implementation. Each queue has its own lock. Two queues are used by comm - request_jobs, for client requests and response_jobs, for server responses.
    
    Issues (to be addressed in future commits):
    1. Code alignment.
    2. Copyright license to be appended.
    3. Support for spinlock locks.

---
+++ xdr/src/ndmp_config.c
@@ -0,0 +1,40 @@
+#include <ndmp_xdr.h>
+
+void ndmp_config_get_host_info(struct client_txn *, XDR*)
+{
+
+}
+
+void ndmp_config_get_connection_type(struct client_txn *, XDR* ) 
+{
+
+}
+
+void ndmp_config_get_auth_attr(struct client_txn *, XDR* ) 
+{
+
+}
+
+void ndmp_config_get_butype_info(struct client_txn *, XDR* ) 
+{
+
+}
+
+void ndmp_config_get_fs_info(struct client_txn *, XDR* ) 
+{
+
+}
+
+void ndmp_config_get_tape_info(struct client_txn *, XDR* ) 
+{
+
+}
+void ndmp_config_get_scsi_info(struct client_txn *, XDR* ) 
+{
+
+}
+
+void ndmp_config_get_server_info(struct client_txn *, XDR* ) 
+{
+
+}
---
+++ xdr/src/ndmp_xdr.h
@@ -0,0 +1,48 @@
+#ifndef __H__NDMP_XDR__
+#define __H__NDMP_XDR__
+
+#include <comm.h>
+#include <rpc/xdr.h>
+
+/*
+ * xdr_decode_encode is the entry point
+ * and callback method for the comm layer 
+ * This is the first upcall made by a worker
+ * thread. 
+ *
+ */
+
+void xdr_decode_encode(struct client_txn *txn);
+
+/*
+ * Each NDMP message will have a message handler that is
+ * responsible for decoding(unmarshaling)  the NDMP XDR request
+ * and invoking the appropriate NDMP action 
+ */
+
+typedef void (* ndmp_message_handler)(struct client_txn *txn, 
+		XDR* request_stream);
+
+/*
+ * ndmp_dispatch determines the message handler for a given
+ * NDMP message type
+ */
+
+ndmp_message_handler ndmp_dispatch(int message_type);
+
+/*
+ *
+ *  The various NDMP message handlers
+ */
+void ndmp_connect_open(struct client_txn *txn, XDR* request_stream);
+void ndmp_connect_close(struct client_txn *, XDR*);
+void ndmp_config_get_host_info(struct client_txn *, XDR*);
+void ndmp_config_get_connection_type(struct client_txn *, XDR* );
+void ndmp_config_get_auth_attr(struct client_txn *, XDR* );
+void ndmp_config_get_butype_info(struct client_txn *, XDR* );
+void ndmp_config_get_fs_info(struct client_txn *, XDR* );
+void ndmp_config_get_tape_info(struct client_txn *, XDR* );
+void ndmp_config_get_scsi_info(struct client_txn *, XDR* );
+void ndmp_config_get_server_info(struct client_txn *, XDR* );
+void ndmp_error_message(struct client *, XDR *); // For wrong message type
+#endif
